f92097645dee49c1f73f14c8f62200c5658a4cc4
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-07-02 12:29:11 +1000

    Implement Speciality Map service. Closes #30

---
+++ pet-clinic-data/src/main/java/com/marcfearby/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.marcfearby.sfgpetclinic.services;
+
+import com.marcfearby.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+
+}
