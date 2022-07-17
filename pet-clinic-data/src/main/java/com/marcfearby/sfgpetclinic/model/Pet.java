1c34e06b66b647949ed85d53d6fd5a4c00291300
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-07-17 19:18:02 +1000

    Convert Pet and Visit classes to JPA. Closes #37

--- pet-clinic-data/src/main/java/com/marcfearby/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/marcfearby/sfgpetclinic/model/Pet.java
@@ -1,10 +1,10 @@
 package com.marcfearby.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Pet extends BaseEntity implements Serializable {
@@ -20,6 +20,9 @@ public class Pet extends BaseEntity implements Serializable {
     private Owner owner;
 
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +54,13 @@ public class Pet extends BaseEntity implements Serializable {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
