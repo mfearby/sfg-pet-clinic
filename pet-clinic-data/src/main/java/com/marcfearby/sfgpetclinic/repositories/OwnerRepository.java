3bbc6d83589d747cbc9049813b4598df5e171e29
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-09-25 14:23:23 +1000

    Implement fix for problem that caused me to downgrade spring boot version previously

--- pet-clinic-data/src/main/java/com/marcfearby/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/marcfearby/sfgpetclinic/repositories/OwnerRepository.java
@@ -2,6 +2,7 @@ package com.marcfearby.sfgpetclinic.repositories;
 
 import com.marcfearby.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,6 +10,8 @@ public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
 
-    List<Owner> findAllByLastNameLike(String lastName);
+    // Adding @Param("lastName") fixes the issue found in videos 215 & 216
+    // and means I can go back to org.springframework.boot version 2.6.7
+    List<Owner> findAllByLastNameLike(@Param("lastName") String lastName);
 
 }
