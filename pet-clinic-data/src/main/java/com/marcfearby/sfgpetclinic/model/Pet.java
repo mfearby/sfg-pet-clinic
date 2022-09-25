19dfcebdd016005a1762f1c2a5765e3bd0ea5707
Marc Fearby <marc.fearby@service.nsw.gov.au>
2022-09-25 17:42:12 +1000

    Finished add/update Pet functionality. Closes #55

--- pet-clinic-data/src/main/java/com/marcfearby/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/marcfearby/sfgpetclinic/model/Pet.java
@@ -1,6 +1,7 @@
 package com.marcfearby.sfgpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -39,6 +40,8 @@ public class Pet extends BaseEntity implements Serializable {
     @JoinColumn(name = "owner_id")  // this table will get an owner_id field with the ID of the associated Owner entity
     private Owner owner;
 
+    @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
--- pet-clinic-web/src/main/java/com/marcfearby/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/com/marcfearby/sfgpetclinic/controllers/PetController.java
@@ -62,7 +62,10 @@ public class PetController {
         if (StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(), true) != null){
             result.rejectValue("name", "duplicate", "already exists");
         }
+
+        pet.setOwner(owner);
         owner.getPets().add(pet);
+
         if (result.hasErrors()) {
             model.put("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
@@ -86,6 +89,7 @@ public class PetController {
             model.addAttribute("pet", pet);
             return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
         } else {
+            pet.setOwner(owner);
             owner.getPets().add(pet);
             petService.save(pet);
             return "redirect:/owners/" + owner.getId();
